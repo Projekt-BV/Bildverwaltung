23b14ef3c7adf980a1388c68b0bb49c3b318aa9c
Tobias Reinert <TobReinert@web.de>
2018-07-10 10:45:25 +0200

    Start fixed

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -9,10 +9,10 @@ import javafx.stage.Stage;
 public class Main extends Application {
 	@Override
 	public void start(Stage stage) throws Exception{
-		Parent root = FXMLLoader.load(getClass().getResource("Main_page_2.4.fxml"));
+		Parent root = FXMLLoader.load(getClass().getResource("/design/Main_page_2.4.fxml"));
 		Scene scene = new Scene(root);
 		
-		String css = Main.class.getResource("application.css").toExternalForm();
+		String css = Main.class.getResource("/design/application.css").toExternalForm();
 		scene.getStylesheets().add(css);
 		stage.setTitle("Genuine Coder");
 		stage.setScene(scene);
