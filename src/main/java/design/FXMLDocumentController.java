ecf56672e501a7b002eed668022a8212617cd229
Tobias Reinert <TobReinert@web.de>
2018-08-16 10:59:02 +0200

    Button link almost completed, grayscale implemented, rotate implemented, Added Exit function to menu bar, updated menu bar.

--- src/main/java/design/FXMLDocumentController.java
+++ src/main/java/design/FXMLDocumentController.java
@@ -12,6 +12,7 @@ import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.event.ActionEvent;
 import java.io.IOException;
+import java.awt.image.BufferedImage;
 
 
 public class FXMLDocumentController implements Initializable {
@@ -52,53 +53,53 @@ public class FXMLDocumentController implements Initializable {
 	
 	@FXML
 	private void browseButtonPressed() {
-
+		System.out.println("I am the browseButtonPressed function");
 	}
 	
 	@FXML
 	private void fullScreenButtonPressed() {
-
+		System.out.println("I am the fullScreenButtonPressed function");
 	}
 	
 	@FXML
 	private void gridButtonPressed() {
-		
+		System.out.println("I am the gridButtonPressed function");
 	}
 	
 	@FXML
 	private void newFolderButtonPressed() {
-
+		System.out.println("I am the newFolderButtonPressed function");
 	}
 
 	@FXML
 	private void filterButtonPressed() {
-
+		System.out.println("I am the filterButtonPressed function");
 	}
 	
 	@FXML
 	private void renameAllButtonPressed() {
-
+		System.out.println("I am the renameAllButtonPressed function");
 	}
 	
 	@FXML
 	private void synchronizeButtonPressed() {
-
+		System.out.println("I am the synchronizeButtonPressed function");
 	}
 	
 	@FXML
 	private void autoSynchroButtonSet() {
-
+		System.out.println("I am the autoSynchroButtonSet function");
 	}
 	
 	@FXML
 	private void dropdownButtonChoiceSelected() {
-
+		System.out.println("I am the dropdownButtonChoiceSelected function");
 	}
 //______________________________________________________________
 
 	@FXML
 	private void sliderMove() {
-
+		System.out.println("I am the sliderMove function");
 	}
 	
 //______________________________________________________________
@@ -108,7 +109,7 @@ public class FXMLDocumentController implements Initializable {
 	//File
 	@FXML
 	private void importImage() {
-
+		System.out.println("I am the importImage function");
 	}
 	
 //	@FXML
@@ -118,76 +119,68 @@ public class FXMLDocumentController implements Initializable {
 	
 	@FXML
 	private void copyImage() {
-
+		System.out.println("I am the copyImage function");
 	}
 	
 	@FXML
 	private void renameImage() {
-
+		System.out.println("I am the renameImage function");
 	}
 	
 	@FXML
 	private void deleteImage() {
-
+		System.out.println("I am the deleteImage function");
 	}
 	
 	@FXML
 	private void saveImage() {
-
+		System.out.println("I am the saveImage function");
 	}
 	
 	@FXML
 	private void saveImageAs() {
-
+		System.out.println("I am the saveImageAs function");
+	}
+	
+	@FXML
+	private void exit() {
+		System.out.println("I am the Exit function");
 	}
 	//-----------------------
 	
 	//Edit
 	@FXML
-	private void rotateLeft() {
-
-	}
-	
-	@FXML
-	private void rotateRight() {
-
-	}
-	
-	@FXML
 	private void blackWhite() {
-
+		System.out.println("I am the blackWhite function");
 	}	
 	
 	@FXML
 	private void cutImage() {
-
+		System.out.println("I am the cutImage function");
 	}	
 	
 	@FXML
 	private void resizeImage() {
-
+		System.out.println("I am the resizeImage function");
 	}
+	
 	//---------------------------
 	
 	//Organize
-//	@FXML
-//	private void newAlbum() {
-//
-//	}	newFolderButtonPressed
-	
+
 	@FXML
 	private void searchImage() {
-
+		System.out.println("I am the searchImage function");
 	}
 	
 	@FXML
 	private void filterImages() {
-
+		System.out.println("I am the filterImages function");
 	}	
 	
 	@FXML
 	private void sortImages() {
-
+		System.out.println("I am the sortImages function");
 	}	
 	
 	//---------------------------
@@ -196,7 +189,7 @@ public class FXMLDocumentController implements Initializable {
 	
 	@FXML
 	private void showDetail() {
-
+		System.out.println("I am the showDetail function");
 	}	
 	
 //	@FXML
@@ -206,12 +199,12 @@ public class FXMLDocumentController implements Initializable {
 	
 	@FXML
 	private void navigator() {
-
+		System.out.println("I am the navigator function");
 	}	
 	
 	@FXML
 	private void information() {
-
+		System.out.println("I am the information function");
 	}
 	
 	//----------------------------
@@ -219,24 +212,58 @@ public class FXMLDocumentController implements Initializable {
 	//Help
 	@FXML
 	private void showCommands() {
-
+		System.out.println("I am the showCommands function");
 	}	
 	
 	@FXML
 	private void changeLanguage() {
-
+		System.out.println("I am the changeLanguage function");
 	}	
 	
 	//--------------------------------------------------------
 	
 	//Edit Mode
 	@FXML
+	private void rotateClockwise() {
+		BufferedImage img = null;
+		//get image to img
+		model.Rotater.rotateClockwise(img);
+		//return img to ImagePane
+	}
+	
+	@FXML
+	private void rotateCounterClockwise() {
+		BufferedImage img = null;
+		//get image to img
+		model.Rotater.rotateAntiClockwise(img);
+		//return img to ImagePane
+	}
+	
+	@FXML
 	private void undo() {
-
+		System.out.println("I am the undo function");
 	}
 	
 	@FXML
 	private void saveMetadata() {
-
+		System.out.println("I am the saveMetadata function");
 	}	
+	
+	@FXML
+	private void filterColor() {
+		System.out.println("I am the filterColor function");
+	}	
+	
+	@FXML
+	private void monochroneButtonPressed() {
+		BufferedImage img = null;
+		//get image to img
+		model.GrayScaler.grayScaleImage(img);
+		//return img to ImagePane
+	}
+	
+	@FXML
+	private void removeFilterButtonPressed() {
+		System.out.println("I am the removeFilter function");
+	}
 }
