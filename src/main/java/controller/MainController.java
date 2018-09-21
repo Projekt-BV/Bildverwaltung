d2b56d1247ec76dcab76dfec2561d2a9c5c55c32
Phillip Persch <phillip.persch91@gmail.com>
2018-09-21 14:25:37 +0200

    memory management (Image objects are huge), multithreading, downscaling of images in grid

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -2,7 +2,6 @@ package controller;
 
 import java.io.IOException;
 import java.text.ParseException;
-import java.util.ArrayList;
 
 import javafx.event.Event;
 import javafx.fxml.FXML;
@@ -11,7 +10,6 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ListView;
-import javafx.scene.image.Image;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
@@ -21,191 +19,180 @@ import model.editing.FileImport;
 
 public abstract class MainController {
 
-	@FXML ListView<Album> listView;
-	@FXML AnchorPane rootPane;	
-	
+	@FXML
+	ListView<Album> listView;
+	@FXML
+	AnchorPane rootPane;
+
 	Database database = new Database();
-	ArrayList<Image> imagesInSelectedAlbum = new ArrayList<Image>();
+
 	static Album selectedAlbum;
 	static boolean didSwitchBack = true;
-	
-	/** 
+
+	/**
 	 * Method to initialize the listView containing the album names.
-	 * @throws ParseException 
+	 * 
+	 * @throws ParseException
 	 */
 	public void initializeListView() {
-		
 		listView.getItems().clear();
-		database.reloadDatabaseContents();			
-		database.getAlbums().stream()
-							.forEach(album -> listView.getItems().add(album));
-		
-		// highlight the selected album		
+		database.reloadDatabaseContents();
+		database.getAlbums().stream().forEach(album -> listView.getItems().add(album));
+
+		// highlight the selected album
 		Album referenceEqualAlbum;
 		if (selectedAlbum != null) {
 			referenceEqualAlbum = listView.getItems().stream()
-											         .filter(album -> album.getName().equals(selectedAlbum.getName()))
-												     .findFirst()
-											         .get();
+					.filter(album -> album.getName().equals(selectedAlbum.getName())).findFirst().get();
 		} else {
 			// if no album is selected, highlight "All Images"
-			referenceEqualAlbum = listView.getItems().stream()
-			         .filter(album -> album.getName().equals("All Images"))
-				     .findFirst()
-			         .get();
+			referenceEqualAlbum = listView.getItems().stream().filter(album -> album.getName().equals("All Images"))
+					.findFirst().get();
 		}
 		listView.getSelectionModel().select(referenceEqualAlbum);
 
-		
 	}
-	
+
 	public Database getDatabase() {
 		return this.database;
 	}
-	
-	
+
 	// Navigation between the main controllers
 	@FXML
-	private void switchScene(Event event) throws IOException{
-			Parent pane = FXMLLoader.load(getClass().getResource("/design/Main_page_edit_mode.fxml"));
-			Scene changePane = new Scene(pane);
-	
-			//Show stage information
-			Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-			window.setScene(changePane);
-			window.show();
-	}
-			
-	@FXML
-	private void switchBack(Event event) throws IOException{
-			selectedAlbum = listView.getSelectionModel().getSelectedItem(); 
-			didSwitchBack = true;
+	private void switchScene(Event event) throws IOException {
+		Parent pane = FXMLLoader.load(getClass().getResource("/design/Main_page_edit_mode.fxml"));
+		Scene changePane = new Scene(pane);
 
-			Parent pane = FXMLLoader.load(getClass().getResource("/design/Main_page_2.4.fxml"));
-			Scene changePane = new Scene(pane);
-			//Show stage information
-			
-			Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-			window.setScene(changePane);
-			window.show();
+		// Show stage information
+		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		window.setScene(changePane);
+		window.show();
 	}
-	
-	
+
+	@FXML
+	private void switchBack(Event event) throws IOException {
+		selectedAlbum = listView.getSelectionModel().getSelectedItem();
+		didSwitchBack = true;
+
+		Parent pane = FXMLLoader.load(getClass().getResource("/design/Main_page_2.4.fxml"));
+		Scene changePane = new Scene(pane);
+		// Show stage information
+
+		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		window.setScene(changePane);
+		window.show();
+	}
+
 	// Menue-Bar----------------------------------------------------
-	
-		//File
-		@FXML
-		private void importImage()  {
-			FileImport tmpImp = new FileImport();
-			Stage window = new Stage();
-			tmpImp.injectMainController((MainControllerGalleryMode)this);
-			tmpImp.start(window);
-		}
-	
-		@FXML
-		private void copyImage() {
-			System.out.println("I am the copyImage function");
-		}
-		
-		@FXML
-		private void renameImage() {
-			System.out.println("I am the renameImage function");
-		}
-		
-		@FXML
-		private void deleteImage() {
-			System.out.println("I am the deleteImage function");
-		}
-		
-		@FXML
-		private void saveImage() {
-			System.out.println("I am the saveImage function");
-		}
-		
-		@FXML
-		private void saveImageAs() {
-			System.out.println("I am the saveImageAs function");
-		}
-		
-		@FXML
-		private void exit() {
-			System.out.println("I am the Exit function");
-		}
-			
-		//Help
-		@FXML
-		private void showCommands() {
-			System.out.println("I am the showCommands function");
-		}	
-			
-		@FXML
-		private void changeLanguage() {
-			System.out.println("I am the changeLanguage function");
-		}	
-		
-		
-		// Buttons below listView
-		@FXML
-		private void addAlbumButtonPressed() throws IOException {
+
+	// File
+	@FXML
+	private void importImage() {
+		FileImport tmpImp = new FileImport();
+		Stage window = new Stage();
+		tmpImp.injectMainController((MainControllerGalleryMode) this);
+		tmpImp.start(window);
+	}
+
+	@FXML
+	private void copyImage() {
+		System.out.println("I am the copyImage function");
+	}
+
+	@FXML
+	private void renameImage() {
+		System.out.println("I am the renameImage function");
+	}
+
+	@FXML
+	private void deleteImage() {
+		System.out.println("I am the deleteImage function");
+	}
+
+	@FXML
+	private void saveImage() {
+		System.out.println("I am the saveImage function");
+	}
+
+	@FXML
+	private void saveImageAs() {
+		System.out.println("I am the saveImageAs function");
+	}
+
+	@FXML
+	private void exit() {
+		System.out.println("I am the Exit function");
+	}
+
+	// Help
+	@FXML
+	private void showCommands() {
+		System.out.println("I am the showCommands function");
+	}
+
+	@FXML
+	private void changeLanguage() {
+		System.out.println("I am the changeLanguage function");
+	}
+
+	// Buttons below listView
+	@FXML
+	private void addAlbumButtonPressed() throws IOException {
+		Stage stage;
+		Parent root;
+		stage = new Stage();
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/design/NewAlbum.fxml"));
+		root = loader.load();
+
+		AlbumController albumController = loader.getController();
+		albumController.injectMainController((MainControllerGalleryMode) this);
+
+		stage.setScene(new Scene(root));
+		stage.setTitle("New Album");
+		stage.show();
+
+	}
+
+	@FXML
+	private void deleteAlbumButtonPressed() {
+		System.out.println("I am the deleteAlbumButtonPressed function");
+	}
+
+	// Bar below Menubar
+
+	@FXML
+	private void browseButtonPressed() {
+		System.out.println("I am the browseButtonPressed function");
+	}
+
+	@FXML
+	private void fullScreenButtonPressed() throws IOException {
+		if (this instanceof MainControllerEditMode) {
 			Stage stage;
 			Parent root;
 			stage = new Stage();
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("/design/NewAlbum.fxml"));
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/design/FullScreen.fxml"));
 			root = loader.load();
-			
-			AlbumController albumController = loader.getController();
-			albumController.injectMainController((MainControllerGalleryMode)this);
-			
+
 			stage.setScene(new Scene(root));
-			stage.setTitle("New Album");
+			stage.setFullScreen(true);
+			stage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
+
+			FullScreenController fullScreenController = loader.getController();
+			fullScreenController.injectMainController((MainControllerEditMode) this);
+			fullScreenController.setExitListener();
+
 			stage.show();
-			
+		} else {
+			Stage stage = (Stage) rootPane.getScene().getWindow();
+			stage.setFullScreen(true);
 		}
-		
-		@FXML
-		private void deleteAlbumButtonPressed() {
-			System.out.println("I am the deleteAlbumButtonPressed function");
-		}
-			
-		
-		// Bar below Menubar
-		
-		@FXML
-		private void browseButtonPressed() {
-			System.out.println("I am the browseButtonPressed function");
-		}
-		
-		@FXML
-		private void fullScreenButtonPressed() throws IOException {
-			if (this instanceof MainControllerEditMode) {
-				Stage stage;
-				Parent root;
-				stage = new Stage();
-				FXMLLoader loader = new FXMLLoader(getClass().getResource("/design/FullScreen.fxml"));
-				root = loader.load();
-				
-				stage.setScene(new Scene(root));
-				stage.setFullScreen(true);
-				stage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
-				
-				FullScreenController fullScreenController = loader.getController();			
-				fullScreenController.injectMainController((MainControllerEditMode)this);
-				fullScreenController.setExitListener();
-				
-				stage.show();	
-			} else {
-				Stage stage = (Stage)rootPane.getScene().getWindow();
-				stage.setFullScreen(true);
-			}
-		}
-		
-		
-		// Bar above gridPane / ImageView
-		@FXML
-		private void dropdownButtonChoiceSelected() {
-			System.out.println("I am the dropdownButtonChoiceSelected function");
-		}
+	}
+
+	// Bar above gridPane / ImageView
+	@FXML
+	private void dropdownButtonChoiceSelected() {
+		System.out.println("I am the dropdownButtonChoiceSelected function");
+	}
 
 }
-	
-
--- src/main/java/controller/MainControllerGalleryMode.java
+++ src/main/java/controller/MainControllerGalleryMode.java
@@ -1,123 +1,132 @@
 package controller;
 
+import java.io.IOException;
 import java.net.URL;
-import java.text.ParseException;
-import java.util.ArrayList;
 import java.util.ResourceBundle;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.GridPane;
-import java.io.IOException;
-import javafx.stage.Stage;
-import javafx.scene.Node;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import model.ImageContainer;
+import model.editing.Resizer;
 
 public class MainControllerGalleryMode extends MainController implements Initializable {
-	
-	@FXML private AnchorPane rootPane;	
-	@FXML private GridPane gridPane;	
-	@FXML private ImageView displayImage;
-		 
-	
+
+	@FXML
+	private AnchorPane rootPane;
+	@FXML
+	private GridPane gridPane;
+	@FXML
+	private ImageView displayImage;
+	private Image imageToDownScale;
+	private int col = 0;
+	private int line = 0;
+
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
-			didSwitchBack = true;
-			initializeListView();
-			initializeGridPane();
+		didSwitchBack = true;
+		initializeListView();
+		initializeGridPane();
 	}
-	
-	
-	/** 
+
+	/**
 	 * Method to initialize the gridPane containing each album's images.
-	 */	
-	@FXML void initializeGridPane() {			
-		
+	 */
+	@FXML
+	void initializeGridPane() {
+
 		gridPane.getChildren().clear(); // clear gridPane
 
-		if (didSwitchBack && selectedAlbum != null){			
-			// if we come from Edit mode, leave selectedAlbum as it is and highlight it in listView
-			
+		if (didSwitchBack && selectedAlbum != null) {
+			// if we come from Edit mode, leave selectedAlbum as it is and highlight it in
+			// listView
+
 			selectedAlbum = database.getAlbums().stream()
-					   							.filter(album -> album.getName().equals(selectedAlbum.getName()))
-					   							.findFirst()
-					   							.get();
-			
+					.filter(album -> album.getName().equals(selectedAlbum.getName())).findFirst().get();
+
 			listView.getSelectionModel().select(database.getAlbums().indexOf(selectedAlbum));
 		} else if (selectedAlbum == null) {
 			// else if no album has been clicked on, get album "All Images"
-			selectedAlbum = database.getAlbums().stream()
-												.filter(album -> album.getName().equals("All Images"))
-												.findFirst()
-												.get();
+			selectedAlbum = database.getAlbums().stream().filter(album -> album.getName().equals("All Images"))
+					.findFirst().get();
 		} else {
 			// else get album that has been clicked on
-			String selectedAlbumName = listView.getSelectionModel().getSelectedItem().getName(); 
-			selectedAlbum = database.getAlbums().stream()
-					.filter(album -> album.getName().equals(selectedAlbumName))
-					.findFirst()
-					.get();
+			String selectedAlbumName = listView.getSelectionModel().getSelectedItem().getName();
+			selectedAlbum = database.getAlbums().stream().filter(album -> album.getName().equals(selectedAlbumName))
+					.findFirst().get();
 
 		}
-		
+
 		didSwitchBack = false;
 
-		
-		// add album's images to collection
-		imagesInSelectedAlbum = new ArrayList<Image>();		
-		selectedAlbum.getImages().stream()
-								 .forEach(i -> imagesInSelectedAlbum.add(new Image(i.getPath())));
-		
-		// add collection to grid
-		int row = 0;
-		int line = 0;
-		for (Image image : imagesInSelectedAlbum) {			
-			ImageView imageView = new ImageView(image);
-			imageView.setFitHeight(130);
-			imageView.setFitWidth(135);
-			
-			gridPane.getChildren().add(imageView);
-			
-			GridPane.setConstraints(imageView, row, line, 1, 1);
-			if (row > 5) {
-				row = 0;
-				line++;
-			} else 
-				row++;	 
+		// downscale all images for performance, load images into grid
+		// Separate thread to keep UI responsive
+		col = 0;
+		line = 0;
+		for (ImageContainer ic : selectedAlbum.getImages()) {
+
+			Platform.runLater(() -> {
+
+				// Call to garbageCollector, because Image takes up a lot of memory
+				this.imageToDownScale = new Image(ic.getPath());
+				System.gc();
+
+				int newWidth = 300;
+				int newHeight = (int) ((newWidth / imageToDownScale.getWidth()) * imageToDownScale.getHeight());
+				imageToDownScale = Resizer.resizeImage(newWidth, newHeight, imageToDownScale);
+
+				ImageView imageView = new ImageView(imageToDownScale);
+				imageView.setFitHeight(130);
+				imageView.setFitWidth(135);
+
+				gridPane.getChildren().add(imageView);
+
+				GridPane.setConstraints(imageView, col, line, 1, 1);
+				if (col > 5) {
+					col = 0;
+					line++;
+				} else
+					col++;
+
+			});
 		}
+
 	}
-	
+
 	@FXML
 	private void gridPaneImagePressed(MouseEvent e) throws IOException {
-		ImageView imageView = (ImageView)e.getPickResult().getIntersectedNode();	
-		
-		MainControllerEditMode.image = imageView.getImage();
-				
-		int index = imagesInSelectedAlbum.indexOf(imageView.getImage());
-		MainControllerEditMode.imageContainer = selectedAlbum.getImages().get(index);
-		
+		ImageView imageView = (ImageView) e.getPickResult().getIntersectedNode();
+
+		int indexOfImageView = gridPane.getChildren().indexOf(imageView);
+		ImageContainer completeImage = selectedAlbum.getImages().get(indexOfImageView);
+
+		MainControllerEditMode.imageContainer = completeImage;
+
 		Parent pane = FXMLLoader.load(getClass().getResource("/design/Main_page_edit_mode.fxml"));
 		Scene changePane = new Scene(pane);
 
-		//Show stage information
-		Stage window = (Stage)((Node)e.getSource()).getScene().getWindow();
+		// Show stage information
+		Stage window = (Stage) ((Node) e.getSource()).getScene().getWindow();
 		window.setScene(changePane);
-		window.show();	
-		
+		window.show();
+
 	}
-	
+
 	public void reloadMainPage() {
 		initializeListView();
 		initializeGridPane();
 	}
-	
+
 	// Bar above gridPane
 	@FXML
 	private void renameAllButtonPressed() {
