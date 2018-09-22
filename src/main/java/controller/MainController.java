d8e11235ba77ae23f66aa8e8ca02e32f8b4bdc76
BlackReaper94 <tobReinert@web.de>
2018-09-22 17:45:41 +0200

    Multiple Language works in Gallery Mode

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -18,12 +18,81 @@ import javafx.stage.Stage;
 import model.Album;
 import model.Database;
 import model.editing.FileImport;
-import java.io.FileInputStream;
-import java.util.Properties;
+
+//GUI Elements
 import java.io.File;
+import javafx.scene.control.*;
+import javafx.scene.text.Text;
+import javafx.scene.control.Label;
+
 
 public abstract class MainController {
 
+	//GUI Elements, hierarchy like the hierarchy in Scenebuilder
+	//North
+	@FXML Menu MenuFile;
+	@FXML MenuItem MenuImport;
+	@FXML MenuItem MenuCopy;
+	@FXML MenuItem MenuRename;
+	@FXML MenuItem MenuDelete;
+	@FXML MenuItem MenuSave;
+	@FXML MenuItem MenuSaveAs;
+	@FXML MenuItem MenuExit;
+	@FXML Menu MenuHelp;
+	@FXML MenuItem MenuAbout;
+	@FXML Menu MenuChangeLanguage;
+	@FXML MenuItem MenuEnglish;
+	@FXML MenuItem MenuGerman;
+	@FXML Button ButtonSearch;
+	@FXML Button ButtonFScreen;
+	@FXML Button ButtonFilter;
+	@FXML TextField TextFieldKeyword;
+	@FXML DatePicker DatePickerFrom;
+	@FXML DatePicker DatePickerTo;
+	//West
+	@FXML Button AddAlbum;
+	@FXML Button DeleteAlbum;
+	//Center
+	@FXML Button ButtonRenameAll;
+	@FXML Button ButtonAddImage;
+	//EditMode
+	//East
+	@FXML Tab TabEditing;
+	@FXML Button TabEditButtonCClock;
+	@FXML Button TabEditButtonClock;
+	@FXML Button TabEditButtonResize;
+	@FXML Text TextWidth;
+	@FXML Button TabEditButtonSetRatio;
+	@FXML Text TextHeight;
+	@FXML Text TextRotate;
+	@FXML Button TabEditButtonCutMode;
+	@FXML Text TextCutImage;
+	@FXML Text TextColorFilter;//
+	@FXML Text TextResizeImage;
+	@FXML Button TabEditButtonUndo;
+	@FXML Button TabEditButtonSave;
+	@FXML Tab TabMetadata;
+	@FXML Button TabMetaButtonSave;
+	@FXML Text TextTitle;
+	@FXML Text TextLocation;
+	@FXML Text TextTags;
+	@FXML Text TextDate;
+	@FXML Label LabelPath;
+	//NewAlbum GUI
+	@FXML Button NewAlbumButtonExit;
+	@FXML Label NewAlbumLabelAlbum;
+	@FXML Button NewAlbumButtonAdd;
+	
+	//Method to rewrite Choicebox
+//			Tab-Editing-ChoiceBox1=kein Filter
+//			Tab-Editing-ChoiceBox2=Schwarz-Weiß
+//			Tab-Editing-ChoiceBox3=Rot
+//			Tab-Editing-ChoiceBox4=Grün
+//			Tab-Editing-ChoiceBox5=Blau
+//			Tab-Editing-ChoiceBox6=Gelb
+//			Tab-Editing-ChoiceBox7=Violet
+//			Tab-Editing-ChoiceBox8=Aqua
+	
 	@FXML
 	ListView<Album> listView;
 	@FXML
@@ -34,6 +103,7 @@ public abstract class MainController {
 	static Album selectedAlbum;
 	static boolean didSwitchBack = true;
 	String currentLanguage = "en";
+	String controller = "GalleryMode";
 	
 	
 	/**
@@ -197,33 +267,108 @@ public abstract class MainController {
 		System.out.println("I am the dropdownButtonChoiceSelected function");
 	}
 
+	@FXML
+	public void changeToGerman()throws IOException {
+		currentLanguage = "de";
+		changeLanguage();
+		ButtonAddImage.setPrefWidth(115);
+		ButtonRenameAll.setLayoutX(510);
+	}
+	
+	@FXML
+	public void changeToEnglish()throws IOException {
+		currentLanguage = "en";
+		changeLanguage();
+		ButtonAddImage.setPrefWidth(98);
+		ButtonRenameAll.setLayoutX(545);
+	}
+	
 	  /**
 		 * Allows to switch between languages
 		 * @author Tobias Reinert
 	     * @throws IOException 
 		 */
-		@FXML
-		public void changeLanguage() throws IOException {
-			Properties config;
-			config = new Properties();
-			FileInputStream fis;
+	@FXML
+	public void changeLanguage() throws IOException {
+		Properties config;
+		config = new Properties();
+		FileInputStream fis;
 		//	FileInputStream fis2;
-			
-			if (currentLanguage == "en") {
-				currentLanguage = "de";
-			} else {
-				currentLanguage = "en";
-			}
-				
-			try {
+		 //Needs to go into the brackets of the Method
+					
+		try {
 			fis = new FileInputStream("C:\\Users\\Tobi\\eclipse-workspace\\Bildverwaltung\\src\\main\\resources\\design\\LangBundle_"+currentLanguage+".properties");
-		//	fis2 = new FileInputStream(System.getProperty("user.dir")+
-		//			"\\eclipse-workspace\\Bildverwaltung\\src\\main\\resources\\design\\LangBundle_de.properties");
+	//	fis2 = new FileInputStream(System.getProperty("user.dir")+
+    //		"\\eclipse-workspace\\Bildverwaltung\\src\\main\\resources\\design\\LangBundle_de.properties");
 			config.load(fis);
 			
-			} catch (IOException io) {
-				io.printStackTrace();
-			}
-			System.out.println(config.getProperty("MenuBar-Language"));
+		} catch (IOException io) {
+			io.printStackTrace();	
 		}
+			
+		//ChangeGui
+		switch (controller) {
+		case("EditMode"):
+			//BorderPane East
+			TabEditing.setText(config.getProperty("Tab-Editing"));
+			TabEditButtonCClock.setText(config.getProperty("Tab-Editing-Button-CClock"));
+			TabEditButtonClock.setText(config.getProperty("Tab-Editing-Button-Clockwise"));
+			TextWidth.setText(config.getProperty("Tab-Editing-Text-Width"));
+			TabEditButtonSetRatio.setText(config.getProperty("Tab-Editing-Button-SetRatio"));
+			TextHeight.setText(config.getProperty("Tab-Editing-TextHeight"));
+			TextRotate.setText(config.getProperty("Tab-Editing-Text-Rotate"));
+			TabEditButtonCutMode.setText(config.getProperty("Tab-Editing-Button-CutMode"));
+			TextCutImage.setText(config.getProperty("Tab-Editing-Text-CutImage"));
+			TextColorFilter.setText(config.getProperty("Tab-Editing-Text-ColorFilter"));
+			TextResizeImage.setText(config.getProperty("Tab-Editing-Text-ResizeImage"));
+			TabEditButtonUndo.setText(config.getProperty("Tab-Editing-Button-Undo"));
+			TabEditButtonSave.setText(config.getProperty("Tab-Editing-Button-Save"));
+			TabMetadata.setText(config.getProperty("Tab-Metadata"));
+			TabMetaButtonSave.setText(config.getProperty("Tab-Metadata-Button-Save"));
+			TextTitle.setText(config.getProperty("Tab-Metadata-Text-Title"));
+			TextLocation.setText(config.getProperty("Tab-Metadata-Text-Location"));
+			TextTags.setText(config.getProperty("Tab-Metadata-Text-Tags"));
+			TextDate.setText(config.getProperty("Tab-Metadata-Text-Date"));
+			LabelPath.setText(config.getProperty("Tab-Metadata-Label-Path"));
+			
+		case("GalleryMode"):
+			//BorderPane North
+			//MenuBar
+			MenuFile.setText(config.getProperty("MenuBar-File"));
+			MenuImport.setText(config.getProperty("MenuBar-File-Import"));
+			MenuCopy.setText(config.getProperty("MenuBar-File-Copy"));
+			MenuRename.setText(config.getProperty("MenuBar-File-Rename"));
+			MenuDelete.setText(config.getProperty("MenuBar-File-Delete"));
+			MenuSave.setText(config.getProperty("MenuBar-File-Save"));
+			MenuSaveAs.setText(config.getProperty("MenuBar-File-SaveAs"));
+			MenuExit.setText(config.getProperty("MenuBar-File-Exit"));
+			MenuHelp.setText(config.getProperty("MenuBar-Help"));
+			MenuAbout.setText(config.getProperty("MenuBar-Help-About"));
+			MenuChangeLanguage.setText(config.getProperty("MenuBar-Help-Language"));
+			MenuEnglish.setText(config.getProperty("MenuBar-Help-Language-English"));
+			MenuGerman.setText(config.getProperty("MenuBar-Help-Language-German"));
+			//North
+			ButtonSearch.setText(config.getProperty("Button-Search"));
+			ButtonFScreen.setText(config.getProperty("Button-FScreen"));
+			ButtonFilter.setText(config.getProperty("Button-Filter"));
+			TextFieldKeyword.setText(config.getProperty("TextField-Keyword"));
+			DatePickerFrom.setAccessibleText(config.getProperty("DatePicker-From"));
+			DatePickerFrom.setAccessibleText(config.getProperty("DatePicker-To"));
+			//BorderPane West
+			AddAlbum.setText(config.getProperty("AddAlbum"));
+			DeleteAlbum.setText(config.getProperty("DeleteAlbum"));
+			
+			if(controller != "GalleryMode") {
+			//BorderPane Center
+			ButtonRenameAll.setText(config.getProperty("Button-RenameAll"));
+			ButtonAddImage.setText(config.getProperty("Button-AddImage"));
+			}
+		break;
+			
+		case("AddAlbum"):		
+			NewAlbumButtonExit.setText(config.getProperty("NewAlbum-Button-Exit"));
+			NewAlbumLabelAlbum.setText(config.getProperty("NewAlbum-Label-Album"));
+			NewAlbumButtonAdd.setText(config.getProperty("NewAlbum-Button-Add"));
+		}
+	}
 }
