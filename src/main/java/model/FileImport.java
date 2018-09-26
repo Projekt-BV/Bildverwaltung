e0a92d59407f506cb381d131e257839a2ce85518
philpsc <phillip.persch91@gmail.com>
2018-09-27 01:27:53 +0200

    added check if image is already in database when trying to import; gallery no longer reloads when no image was selected in file chooser

--- src/main/java/model/FileImport.java
+++ src/main/java/model/FileImport.java
@@ -4,19 +4,17 @@ import java.io.File;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
-import controller.MainController;
 import controller.MainControllerGalleryMode;
 import database.SendSQLRequest;
 import javafx.application.Application;
 import javafx.stage.FileChooser;
-
 import javafx.stage.Stage;
 
 public class FileImport extends Application {
@@ -27,15 +25,32 @@ public class FileImport extends Application {
     public void start(final Stage stage) {
         stage.setTitle("File import");
  
-        final FileChooser fileChooser = new FileChooser();
+        final FileChooser fileChooser = new FileChooser();  
+        
         
         List<File> list = fileChooser.showOpenMultipleDialog(stage);
+        
+        // List of paths of all images in database, so that an existing image cannot be added again.
+        // Storing the same picture twice would cause problems.
+        List<String> allImagePaths = mainController.getDatabase()
+        										   .getAlbums()
+        										   .get(0)
+        										   .getImages()
+        										   .stream()
+        										   .map(i -> i.getPath())
+        										   .collect(Collectors.toList());
+        
         if (list != null) {
         	for (File file : list) {
-        		openFile(file);
+        		
+        		String path = ("file:///" + file.getAbsolutePath()).replace("\\", "/");
+        		
+        		if (allImagePaths.isEmpty() || !allImagePaths.contains(path)) {
+        			openFile(file);
+        		}
             }
+            mainController.reloadMainPage();
         }  
-        mainController.reloadMainPage();
     }         
  
     private void openFile(File file) {
